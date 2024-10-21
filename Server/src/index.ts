7142e814ee5a87091896338f98a0b9b5b4292308
alielmayyah <alielmayyah@gmail.com>
2024-10-21 22:28:34 +0300

    commit number 10

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -12,12 +12,8 @@ const corsOptions = {
 };
 const user_route = require("../src/routes/user_routes");
 const app = express();
-<<<<<<< HEAD
-const PORT = process.env.PORT || 4000;
-=======
 const PORT = process.env.SERVER_PORT;
 app.use(cors(corsOptions));
->>>>>>> b993c392d3af055d07ec3aeaf037d32289d98384
 app.use(express.json());
 app.use(user_route);
 app.get("/", (req, res) => {
